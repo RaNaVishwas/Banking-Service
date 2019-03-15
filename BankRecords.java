619e8df24990270f70a4bdb23d69e1fe99d9cde1
Vishwas RN <vreddynaga@hawk.iit.edu>
2019-03-15 17:08:07 -0500

    Update BankRecords.java

--- BankRecords.java
+++ BankRecords.java
@@ -85,3 +85,128 @@ public class BankRecord extends Client {
 	public void set_Pep_lab3(String pep_lab3) {
 		this.pep_lab3 = pep_lab3;
 	}
+
+	/** 
+	 * Get method for ID
+	 * @return : String
+	 */
+	public String getId_lab3() {
+		return id_lab3;
+	}
+	
+	/**
+	 * Set Method for ID
+	 * @param : id
+	 */
+
+	public void setId(String id) {
+		this.id_lab3 = id;
+	}
+/**
+ * Get method for age
+ * @return : int
+ */
+public int getAge_lab3() {
+		return age_lab3;
+	}
+/**
+ * Set method for age
+ * @param: age
+ */
+	public void setAge(int age) {
+		this.age_lab3 = age;
+	}
+/**
+ * Get method for sex
+ * @return : string
+ */
+
+	public String getSex_lab3() {
+		return sex_lab3;
+	}
+
+	
+/**
+ * Set method for sex
+ * @param : sex
+ */
+
+	public void setSex(String sex) {
+		this.sex_lab3 = sex;
+	}
+/**
+ * Get message for region
+ * @return : String
+*/ 
+	public String getRegion_lab3_Reg() {
+		return region_lab3;
+	}
+/**
+ * set message for region
+ * @param : region
+ */
+	public void setRegion(String region) {
+		this.region_lab3 = region;
+	}
+/**
+ * get method for Income
+ * @return : Double
+ */
+	public double get_Income_lab4() {
+		return income_lab3;
+	}
+/**
+ * Set method for income
+ * @param : income
+ */
+	public void setIncome(double income) {
+		this.income_lab3 = income;
+	}
+	
+	public static BankRecord[] getBrObj() {
+		return bankObject_lab;
+	}
+
+	public static List<List<String>> getArray() {
+		return arr_ay_lab3;
+	}
+
+	/**
+	 * This is the main method. 
+	 * @param args : string[]
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankRecord Bank_Record_lab = new BankRecord();
+		
+		try {
+			Bank_Record_lab.read();
+			Bank_Record_lab.process();
+			//br.print();
+		} catch (FileNotFoundException e) {
+			System.out.println("File is not located!");
+		} catch (IOException e) {
+			System.out.println("File is not located!");
+		}
+		String time_stamp_lab3 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+				.format(Calendar.getInstance().getTime());
+		System.out.println("Dated on :" + time_stamp_lab3
+				+ "\nProgrammed by Student Vishwas\n");
+	}
+
+	/**
+	 * The read method reads the data from the csv file and stores it in a array list
+	 */
+	@Override
+	
+	public void read() throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub - processing data
+		String line = "";
+		BufferedReader BufferRead_lab = new BufferedReader(new FileReader("bank-Detail.csv"));
+		while ((line = BufferRead_lab.readLine()) != null) {
+			arr_ay_lab3.add(Arrays.asList(line.split(",")));
+		}
+	}
+/**
+ * The print method displays the first 25 records and displays data along with the header
+ */
